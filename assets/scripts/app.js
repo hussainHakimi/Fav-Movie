e836117e076517f14ce7a403ba081900082b38cc
hussainHakimi <hussainbayan614@gmail.com>
2022-04-13 19:44:30 +0430

    Delete movie with clicking on that.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,12 +18,25 @@ const updateUI = () => {
     }
 };
 
-
-const deleteMovieHandler = () => {
+const deleteMovie = () => {
     
+}
+
+
+const deleteMovieHandler = (movieId) => {
+    let movieIndex = 0;
+    for(const movie of movies){
+        if(movie.id === movieId){
+            break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex, 1); // splice will delete form an array.
+    const listRoot = document.getElementById('movie-list');
+    listRoot.children[movieIndex].remove();
 };
 
-const newMovieElementDetails = (title, imageUrl, rating) => {
+const newMovieElementDetails = (id, title, imageUrl, rating) => {
     const newMovieElement = document.createElement('li');
     newMovieElement.className = "movie-element";
     newMovieElement.innerHTML = `
@@ -35,7 +48,7 @@ const newMovieElementDetails = (title, imageUrl, rating) => {
         <p>${rating}/5 stars</p>
     </div>
     `;
-    newMovieElement.addEventListener('click', deleteMovieHandler);
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
     const listRoot = document.getElementById('movie-list');
     listRoot.append(newMovieElement);
 };
@@ -73,6 +86,7 @@ const addMovieHandler = () => {
         alert('Please enter a Valid value! (rating between 1 to 5)');
     }
     const newMovie = {
+        id : Math.random().toString(),
         title : titleValue,
         image : imageUrlValue,
         rating : ratingValue
@@ -82,7 +96,7 @@ const addMovieHandler = () => {
     console.log(movies);
     toggleMovieModal();
     clearMovieInputs();
-    newMovieElementDetails(newMovie.title, newMovie.image, newMovie.rating);
+    newMovieElementDetails(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
     updateUI();
 };
 
